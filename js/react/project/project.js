6e51af9de8dca31985743f226c7370d0b3c86155
Ahmed9190 <hamadawaled11@gmail.com>
2021-08-09 18:17:49 +0200

    finex projectdetails.html conflict

--- js/react/project/project.js
+++ js/react/project/project.js
@@ -6,26 +6,45 @@ const { axios } = window;
 const baseURL = `http://localhost:8000`;
 axios.defaults.baseURL = `${baseURL}/api`;
 
+const translation = {
+  en: {
+    titlehead: "About the project",
+    locationhead: "The Location of the Project",
+    // Home: "Home",
+    // projecttitle: "Projects",
+    Description: "Simple Description",
+  },
+  ar: {
+    titlehead: "معلومات عن المشروع",
+    locationhead: "موقع المشروع",
+    // Home: "الصفحة الرئيسية",
+    // projecttitle: "المشاريع",
+    Description: "الوصف",
+  },
+};
+
 const Project = () => {
   const [project, setProject] = React.useState([]);
 
   React.useEffect(() => {
     const id = window.location.hash.substring(1);
     axios.get(`/projects/${id}`).then(({ data }) => {
-      console.log(data);
+      console.log(data.title);
       setProject(data);
-      setProjectTitle(
-        currentLanguage == "en" ? data.title : data.title_a,
-        currentLanguage == "en" ? "About the project" : "تفاصيل عن المشروع"
-      );
+      if (data.title || data.title_a)
+        setProjectTitle(currentLanguage == "en" ? data.title : data.title_a);
     });
   }, []);
   return (
     <>
       <Description
-        desriptionTitle={currentLanguage == "en" ? "Description" : "الوصف"}
         description={
           currentLanguage == "en" ? project.description : project.description_a
+        }
+        location={
+          currentLanguage == "en"
+            ? project.short_description
+            : project.short_description_a
         }
       />
       <Images images={project?.images?.length > 0 ? project.images : []} />
@@ -33,25 +52,43 @@ const Project = () => {
   );
 };
 
-const Description = ({ desriptionTitle, description }) => {
+const Description = ({ description, location }) => {
   return (
     <div className="container">
       <div className="row justify-content-center">
         <div className="col-lg-7 text-center">
           <div className="section-title">
-            <span id="simpdesc" className="h6 text-color">
-              {desriptionTitle}
-            </span>
-            <p className="mt-5">{description}</p>
+            <span id="locationhead" className="h6 text-color"></span>
+            <h2
+              className="mt-3 content-title"
+              dangerouslySetInnerHTML={{ __html: location }}
+            ></h2>
+          </div>
+          <div className="section-title">
+            <span id="desc" className="h6 text-color"></span>
+            <p
+              className="mt-5"
+              dangerouslySetInnerHTML={{ __html: description }}
+            ></p>
           </div>
         </div>
+        {/*             
+        <div class="section-title">
+          <span id="locationhead" class="h6 text-color"></span>
+          <h2 class="mt-3 content-title">{location}</h2>
+        </div>
+        <div className="col-lg-7 text-center">
+          <div className="section-title">
+            <span id="simpdesc" className="h6 text-color"></span>
+            <p className="mt-5">{description}</p>
+          </div>
+        </div> */}
       </div>
     </div>
   );
 };
 
 const Images = ({ images }) => {
-  console.log(images);
   return (
     <div className="container">
       <div className="row portfolio-gallery d-flex justify-content-center">
@@ -89,11 +126,26 @@ const Images = ({ images }) => {
   );
 };
 
-const setProjectTitle = (title, about) => {
+const setProjectTitle = (title) => {
+  //   titlehead
+  // locationhead
+  // Description
+  document.querySelector("#titlehead").innerHTML =
+    translation[currentLanguage]["titlehead"];
   document.querySelector(
     "body > div > section.page-title.projectheadbg > div > div > div > div > h1"
   ).innerHTML = title;
-  document.querySelector("#titlepage").innerHTML = about;
+
+  document.querySelector("#locationhead").innerHTML =
+    translation[currentLanguage]["locationhead"];
+  document.querySelector("#desc").innerHTML =
+    translation[currentLanguage]["Description"];
+  // document.querySelector(
+  //   "body > div > section.page-title.projectheadbg > div > div > div > div > h1"
+  // ).innerHTML = title;
+  // document.querySelector("#titlepage").innerHTML = about;
+
+  // document.querySelector("#locationhead").innerHTML =
 };
 
 ReactDOM.render(<Project />, document.querySelector("#react-root-project"));
